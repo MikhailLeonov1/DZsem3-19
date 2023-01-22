1b849e2f3e4e2ed07e71610180724e1a18d5223a
user name <you@example.com>
2023-01-22 22:24:55 +0300

    дз номер 19

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите пятизначное число: "); // Ввод
+string num = Console.ReadLine();
+
+if (num.Length == 5)
+{
+    if (num[0] == num[4] && num[1] == num[3]) // Проверка числа на палиндром
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Число не является пятизначным");
+}
